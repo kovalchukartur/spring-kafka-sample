fc021a94ffe15ed82f76770f2504f18669252786
Artur Kovalchuk <arkovalchuk@playtika.com>
2022-11-29 01:25:47 +0200

    Init

---
+++ src/main/java/ua/kovalchuk/springkafkasample/SpringKafkaSampleApplication.java
@@ -0,0 +1,13 @@
+package ua.kovalchuk.springkafkasample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaSampleApplication.class, args);
+    }
+
+}
